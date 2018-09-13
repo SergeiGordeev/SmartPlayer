c7cc5a9b1895b0c975d66f7bf921f562cf3660ab
Sergey <gordeevs85@mail.ru>
2018-09-14 00:20:05 +0400

    Go to game page method was added.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,9 +8,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import utils.Constants;
 
 import java.util.List;
+
+import static utils.Constants.*;
 
 public class Main
 {
@@ -24,17 +25,31 @@ public class Main
         Main mainInstance = injector.getInstance(Main.class);
 
         driver = mainInstance.chrome.getDriver();
-        driver.manage().window().setPosition(new Point(0, 950));
-        driver.get(Constants.DOMAIN_PATH);
+        driver.manage().window().setPosition(new Point(0, 920));
+        driver.get(DOMAIN_PATH);
         driver.manage().deleteAllCookies();
 
-        List<WebElement> elements = driver.findElements(By.className("fSports"));
+        List<WebElement> elements = driver.findElements(By.className(CLASS_NAME_SELECTOR));
+
+        goToGamePage(elements);
+        driver.quit();
+    }
+
+    private static void goToGamePage(List<WebElement> elements)
+    {
         for(WebElement element : elements)
         {
-            System.out.println(element.getAttribute("title"));
-            System.out.println(element.getAttribute("data-href"));
-            System.out.println("\n");
+            if(null != element.getAttribute("title"))
+            {
+                String title = element.getAttribute("title");
+                String gameUrl = element.getAttribute("data-href");
+
+                if (title.equals(GAME_NAME_EN) || title.equals(GAME_NAME_RU))
+                {
+                    driver.get(driver.getCurrentUrl() + gameUrl);
+                    return;
+                }
+            }
         }
-        driver.quit();
     }
 }
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -4,6 +4,10 @@ public class Constants {
     public static final String CHROME_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\chromedriver.exe";
     public static final String FIRE_FOX_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\geckodriver.exe";
     public static final String INTERNET_EXPLORER_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\IEDriverServer.exe";
+
 //    public static final String DOMAIN_PATH = "https://1xdxt.host/";
     public static final String DOMAIN_PATH = "https://1xqwk.host/";
+    public static final String CLASS_NAME_SELECTOR = "fSports";
+    public static final String GAME_NAME_RU = "Волейбол";
+    public static final String GAME_NAME_EN = "Volleyball";
 }
