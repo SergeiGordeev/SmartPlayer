5fa1bc150b4462f4da7fff3ff7480ca428cad5fe
Sergey <gordeevs85@mail.ru>
2018-09-13 23:27:50 +0400

    Get necessary game elements.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,21 +4,13 @@ import com.google.inject.Injector;
 import driver.BrowserDriver;
 import injector.InjectorModule;
 import injector.annotations.Chrome;
-import org.apache.commons.lang3.StringUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import utils.Constants;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.net.URL;
 import java.util.List;
-
-import static utils.Constants.DOWNLOADED_IMAGES_LOCAL_PATH;
-import static utils.Constants.JPG;
 
 public class Main
 {
@@ -35,36 +27,14 @@ public class Main
         driver.manage().window().setPosition(new Point(0, 950));
         driver.get(Constants.DOMAIN_PATH);
         driver.manage().deleteAllCookies();
-        firstTest(5);
+
+        List<WebElement> elements = driver.findElements(By.className("fSports"));
+        for(WebElement element : elements)
+        {
+            System.out.println(element.getAttribute("title"));
+            System.out.println(element.getAttribute("data-href"));
+            System.out.println("\n");
+        }
         driver.quit();
-    }
-
-    public static void firstTest(int countOfImagesForDownload)
-    {
-        List<WebElement> imgElementsList  = driver.findElements(By.xpath("//li[@class='  js-content-li']/a/span[@class='image']/img"));
-        for(WebElement img : imgElementsList)
-        {
-            downloadImage(img.getAttribute("data-src"), img.getAttribute("alt"), JPG);
-            countOfImagesForDownload--;
-            if(countOfImagesForDownload == 0)
-            {
-                return;
-            }
-        }
-        System.out.println(imgElementsList.size());
-    }
-
-    public static void downloadImage(String url, String imageName, String imageType)
-    {
-        try
-        {
-            ImageIO.write(ImageIO.read
-                            (new URL(StringUtils.substringBefore(url, imageType) + imageType)),
-                            imageType,
-                            new File(DOWNLOADED_IMAGES_LOCAL_PATH + imageName + "." + imageType));
-        } catch (IOException e)
-        {
-            e.printStackTrace();
-        }
     }
 }
--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -3,11 +3,7 @@ package utils;
 public class Constants {
     public static final String CHROME_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\chromedriver.exe";
     public static final String FIRE_FOX_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\geckodriver.exe";
-
-    //TODO: set correct driver for below paths from https://www.seleniumhq.org/download/
     public static final String INTERNET_EXPLORER_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\IEDriverServer.exe";
-    public static final String DOMAIN_PATH = "https://www.ivi.ru/";
-
-    public static final String JPG = "jpg";
-    public static final String DOWNLOADED_IMAGES_LOCAL_PATH = "C:\\Users\\Сергей\\pic\\";
+//    public static final String DOMAIN_PATH = "https://1xdxt.host/";
+    public static final String DOMAIN_PATH = "https://1xqwk.host/";
 }
