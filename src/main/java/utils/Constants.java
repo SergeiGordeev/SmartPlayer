5334ee74d6165312fc5e73da49e985ef10bb5e18
SergeyGordeev <machine.code.senior@gmail.com>
2018-10-09 13:49:00 +0400

    Add JSONDataSender

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,15 +1,14 @@
 package utils;
 
 public class Constants {
+    public static final String DATA_VIEWER_PAGE = "http://a.masteryodo.tmweb.ru/";
+
     public static final String CHROME_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\chromedriver.exe";
     public static final String FIRE_FOX_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\geckodriver.exe";
     public static final String INTERNET_EXPLORER_WEB_DRIVER_LOCAL_PATH = "src\\main\\resources\\IEDriverServer.exe";
 
-//    public static final String DOMAIN_PATH = "https://1xdxt.host/";
-    public static final String DOMAIN_PATH = "https://1xqwk.host/";
+    public static final String DOMAIN_PATH = "https://1xstavka.ru/ru/live/Volleyball/";
     public static final String CLASS_NAME_SELECTOR = "fSports";
-//    public static final String GAME_NAME_RU = "Волейбол";
-//    public static final String GAME_NAME_EN = "Volleyball";
 
     public static final String GAME_NAME_RU = "Футбол";
     public static final String GAME_NAME_EN = "Football";
